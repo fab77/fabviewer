df6f528630b0d93746b7aae9815d2694b858d25b
Henrik Norman <honke.norman@gmail.com>
2020-11-19 11:50:04 +0100

    Removed one healpix grid option

--- src/js/model/HealpixGridTileDrawer.js
+++ src/js/model/HealpixGridTileDrawer.js
@@ -3,8 +3,6 @@
 import global from '../Global';
 import { tileBufferSingleton } from './TileBuffer';
 
-const SHOW_ONLY_FULLY_LOADED_TILES = true;
-
 class HealpixGridTileDrawer {
 
 	constructor() {
@@ -115,10 +113,6 @@ class HealpixGridTileDrawer {
 	}
 
 	drawTile(tile){
-		if(SHOW_ONLY_FULLY_LOADED_TILES){
-			let correspondingTile = tileBufferSingleton.getTile(tile.order, tile.ipix);
-			if(!correspondingTile.textureLoaded){return;}
-		}
 
 		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, tile.vertexPositionBuffer);
 		this.gl.vertexAttribPointer(tile.vertexPositionAttribute, 3, this.gl.FLOAT, false, 0, 0);
