e0e2c579784f4a7f8bc7501705e50c8e77db8a25
Fabrizio Giordano <fabriziogiordano77@gmail.com>
2021-02-07 19:19:17 +0100

    missing MouseHelper class

---
+++ src/js/utils/MouseHelper.js
@@ -0,0 +1,112 @@
+"use strict";
+/**
+ * @author Fabrizio Giordano (Fab)
+ */
+
+import Healpix from "healpixjs";
+import {Vec3, Pointing} from "healpixjs";
+
+class MouseHelper {
+	
+	xyz;
+	raDec;
+	phiTheta;
+	
+	/**
+	 * @param xyz array [x, y, z] 
+	 * @param raDecDeg [ra, dec] in degrees in equatorial J2000 
+	 * @param phiThetaDeg [phi, theta] in degrees spherical coords
+	 */
+	constructor(in_xyz, in_raDecDeg, in_phiThetaDeg) {
+		if (in_xyz != null && in_xyz !== undefined){
+			this.xyz = in_xyz;
+		}
+		
+		if (in_raDecDeg != null && in_raDecDeg !== undefined){
+			this.raDec = in_raDecDeg;
+		}
+		
+		if (in_phiThetaDeg != null && in_phiThetaDeg !== undefined){
+			this.phiTheta = in_phiThetaDeg;
+		}
+	};
+	
+	computeNpix256() {
+		
+		if (this.xyz != null){
+			let healpix256 = new Healpix(256);
+			let vec3 = new Vec3(this.x, this.y, this.z);
+			let pointing = new Pointing(vec3);
+			let res = healpix256.ang2pix(pointing);
+//			console.log(res);
+			return res;
+		}
+		return null;
+		
+	};
+	
+	clear(){
+		
+		this.xyz = null;
+		this.raDec = null;
+		this.phiTheta = null;
+	};
+	
+	set xyz(in_xyz){
+		if (in_xyz != null && in_xyz !== undefined){
+			this.xyz = in_xyz;
+		}
+	};
+	
+	set raDecDeg(in_raDecDeg){
+		if (in_raDecDeg != null && in_raDecDeg !== undefined){
+			this.raDec = in_raDecDeg;
+		}
+		
+	};
+	
+	set phiThetaDeg(in_phiThetaDeg){
+		if (in_phiThetaDeg != null && in_phiThetaDeg !== undefined){
+			this.phiTheta = in_phiThetaDeg;
+		}
+		
+	};
+	
+	
+	
+	get xyz() {
+		return this.xyz;
+	};
+	
+	get x(){
+		return this.xyz[0];
+	};
+	
+	get y(){
+		return this.xyz[1];
+	};
+	
+	get z(){
+		return this.xyz[2];
+	};
+	
+	get ra(){
+		return this.raDec[0];
+	};
+	
+	get dec(){
+		return this.raDec[1];
+	};
+	
+	get phi(){
+		return this.phiTheta[0];
+	};
+	
+	get theta(){
+		return this.phiTheta[1];
+	};
+	
+	
+}
+
+export default MouseHelper;
