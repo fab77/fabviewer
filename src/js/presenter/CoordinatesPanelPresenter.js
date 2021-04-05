fbac656c38bc489366f769098fe5b7285249cab7
Fabrizio Giordano <fabriziogiordano77@gmail.com>
2021-04-05 18:46:40 +0200

    Added logic to hide and show spherical coords in the coordsPanel (top of the canvas)

--- src/js/presenter/CoordinatesPanelPresenter.js
+++ src/js/presenter/CoordinatesPanelPresenter.js
@@ -9,12 +9,16 @@ class CoordinatesPanelPresenter {
 
 	_parentView;
 	_view;
+	_showSphericalCoords;
 	
 	constructor(in_parentView){
 		
 		this._parentView = in_parentView;
 		this._view = new CoordinatesPanelView();
 		this._parentView.append2Body(this._view.getHtml());
+		this._showSphericalCoords = false;
+		this._view.showSphericalCoords(this._showSphericalCoords); 
+
 	}
 	
 	get view(){
@@ -22,7 +26,12 @@ class CoordinatesPanelPresenter {
     }
 	
 	update(in_raDecDeg, in_raHMS, in_decDMS){
-		this.view.setModel(in_raDecDeg, in_raHMS, in_decDMS);
+		this.view.setModel(in_raDecDeg, in_raHMS, in_decDMS, this._showSphericalCoords);
+	}
+	
+	showSphericalCoords(show){
+		this._showSphericalCoords(show);
+		this._view.showSphericalCoords(show); 
 	}
 	
 	
--- src/js/view/CoordinatesPanelView.js
+++ src/js/view/CoordinatesPanelView.js
@@ -5,6 +5,7 @@ class CoordinatesPanelView{
  
 	_html;
 	_model;
+	_showSphericalCoords;
 	
     constructor(){
     	
@@ -15,8 +16,8 @@ class CoordinatesPanelView{
             getHtml: ()=>{
                 return this._html;
             },
-            setModel: (in_raDecDeg, in_raHMS, in_decDMS)=>{
-            	
+            setModel: (in_raDecDeg, in_raHMS, in_decDMS, phi, theta)=>{
+
             	$('#raDecDeg').html(in_raDecDeg.ra.toFixed(4)+' '+in_raDecDeg.dec.toFixed(4));
             	
             	let sign = '+';
@@ -24,15 +25,30 @@ class CoordinatesPanelView{
         			sign = '';
         		}
             	$('#raDecHms').html(in_raHMS.h +" "+in_raHMS.m +" "+in_raHMS.s.toFixed(2)+ " "+sign+in_decDMS.d+" "+in_decDMS.m+" "+in_decDMS.s.toFixed(2));
-            }
+            	
+            	if(this._showSphericalCoords){
+            		$('#phiThetaDeg').html(phi.toFixed(4) +" "+theta.toFixed(4));
+            	}
+            },
+            showSphericalCoords: (show) => {
+            	this._showSphericalCoords = show;
+            	if (!this._showSphericalCoords){
+            		$('#phiTheta').css("display", "none");
+            	}else{
+            		$('#phiTheta').css("display", "block");
+            	}
+            } 
         }
         return _public;
     }
  
+    
+    
     init(){
     	this._html = $("<div id='coordsContainer'>" +
     			"<div>RA/Dec (degrees) <span id='raDecDeg'></span></div>" +
     			"<div>RA/Dec (hms/dms) <span id='raDecHms'></span></div>" +
+    			"<div id='phiTheta'>Phi/Theta (degs) <span id='phiThetaDeg'></span></div>" +
     			"</div>");
     }
 }
